cfabf27a3248957a93860d832eba36b3b6817365
Nesar K.S <96533558+nesarks@users.noreply.github.com>
2023-01-05 19:25:33 +0530

    Add files via upload
    Java program to read two integers a and b. Compute a/b and print, when b is not zero.
    Raise an exception when b is equal to zero.

---
+++ division.java
@@ -0,0 +1,23 @@
+//Java program to read two integers a and b. Compute a/b and print, when b is not zero.
+Raise an exception when b is equal to zero. 
+
+import java.util.Scanner;
+class division
+{
+public static void main(String[] args)
+{
+int a,b,result;
+Scanner input =new Scanner(System.in);
+System.out.println("Input two integers");
+a=input.nextInt();
+b=input.nextInt();
+try
+{
+result=a/b;
+System.out.println("Result="+result);
+}
+catch(ArithmeticException e)
+{
+System.out.println("exception caught: Divide by zero error"+e);
+}
+}
